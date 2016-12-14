6fcfa28f8df6e10eb9d3c1cf556537268b5031d5
erurai <shameimaruaya-gensou@docomo.ne.jp>
2016-12-14 12:08:09 +0900

    山岸プロジェクトの結合

---
+++ Assets/Scripts/Yamagishi/BallMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMove : MonoBehaviour
+{
+    public float speed = 1;
+
+	// Use this for initialization
+	void Start ()
+    {
+        this.GetComponent<Rigidbody>().AddForce(
+    (transform.forward + transform.right) * speed,
+    ForceMode.VelocityChange);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Yamagishi/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Transform target;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        //自分と相手の相対座標を求める
+        offset = GetComponent<Transform>().position - target.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //自分の座標にtargetの座標を取得
+        GetComponent<Transform>().position = target.position + offset;
+
+	}
+}
