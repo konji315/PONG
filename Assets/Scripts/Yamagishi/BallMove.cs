786fd43aef5387d93f8b95b566e9988a6a8765bd
erurai <shameimaruaya-gensou@docomo.ne.jp>
2016-12-15 16:50:33 +0900

    ボールのプレハブ追加　ゴールの判定追加(判定のみでエフェクト等は未実装)

--- Assets/Scripts/Yamagishi/BallMove.cs
+++ Assets/Scripts/Yamagishi/BallMove.cs
@@ -4,18 +4,22 @@ using System.Collections;
 public class BallMove : MonoBehaviour
 {
     public float speed = 1;
+    private int count = 0;
 
 	// Use this for initialization
 	void Start ()
     {
-        this.GetComponent<Rigidbody>().AddForce(
-    (transform.forward + transform.right) * speed,
-    ForceMode.VelocityChange);
     }
 
     // Update is called once per frame
     void Update ()
     {
-
+        count++;
+        if (count == 60)
+        {
+            this.GetComponent<Rigidbody>().AddForce(
+                (transform.forward + transform.right) * speed,
+                ForceMode.VelocityChange);
+        }
     }
 }
---
+++ Assets/Scripts/Yamagishi/GoalScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalScript : MonoBehaviour {
+
+    //トリガーとの接触時に呼び出されるコールバック
+    void OnTriggerEnter(Collider hit)
+    {
+        //接触対象はプレイヤータグですか？
+        if (hit.tag == ("Ball"))
+        {
+            
+        }
+    }
+}
