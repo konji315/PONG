b5ab30439af5d8a90f4cd4f6ee9ed20fb2535ebc
shou060403 <shou060403@yahoo.co.jp>
2016-12-17 20:38:57 +0900

    タイトルシーン（完成版）の追加

---
+++ Assets/Scripts/Iriyama/ChangeScene_Select.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//シーン移動のための名前領域
+using UnityEngine.SceneManagement;
+
+public class ChangeScene_Select : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //何かのキーが押されたら
+        if (Input.anyKey)
+        {
+            //シーン移動
+            SceneManager.LoadScene("PaddleSelectScene");
+
+        }
+    }
+}
